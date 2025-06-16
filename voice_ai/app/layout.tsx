a145a1943cf454098004caf7635abde20d05c204
NITHIN B M <nithinbmpadthare90@gmail.com>
2025-06-16 20:54:37 +0530

    added auth file

--- voice_ai/app/layout.tsx
+++ voice_ai/app/layout.tsx
@@ -3,7 +3,7 @@ import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
 const monaSans = Mona_Sans({
-  variable: "--font-geist-sans",
+  variable: "--font-mona-sans",
   subsets: ["latin"],
 });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="Dark">
+    <html lang="en" className="dark">
       <body
         className={`${monaSans.className} antialiased`}
       >
